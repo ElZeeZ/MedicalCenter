0f26b5e5762ab7561b97820c61a10a3dcef9d91a
ElZeeZ <zein.zeez@gmail.com>
2024-12-21 11:30:27 +0200

    12/21/2024

--- index.php
+++ index.php
@@ -189,9 +189,5 @@ Al-Riyad Bldg - 1st Floor</p>
     <a href="login.php">Log In</a>
   </div>
 </footer>
-
-
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
   </body>
 </html>
